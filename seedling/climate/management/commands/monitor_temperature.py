feeffc9198f9958fb1fa511f2ded0fb51af6be10
James Stevenson <james.m.stevenson@gmail.com>
2023-01-02 22:54:37 -0500

    Fix monitoring mgmt command.

--- seedling/climate/management/commands/monitor_temperature.py
+++ seedling/climate/management/commands/monitor_temperature.py
@@ -3,9 +3,8 @@ import time
 
 from django.core.management.base import BaseCommand, CommandError
 
-
 from seedling.climate.models import ClimateReading
-from seedling.climate.sensor import sample_humidity_and_temperature, get_humidity_and_temperature
+from seedling.climate.sensor import sample_humidity_and_temperature
 
 
 class Command(BaseCommand):
@@ -15,13 +14,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             while True:
-                time.sleep(60 * 5)
-                readings = get_humidity_and_temperature()
+                readings = sample_humidity_and_temperature()
                 if readings:
                     humidity, temperature = readings
-                    self.stdout.write(self.style.NOTICE(f"Humidity is ${humidity:.2f} and temp is {temperature:.2f}."))
+                    temperature_f = ((9 / 5) * temperature) + 32
+                    self.stdout.write(self.style.NOTICE(f"Humidity is {humidity:.2f}% and temp is {temperature_f:.2f}°F."))
                 else:
                     self.stdout.write(self.style.ERROR("Failed to get readings from sensor."))
+                time.sleep(60 * 5)
         except KeyboardInterrupt:
             self.stdout.write(self.style.SUCCESS("Exiting."))
             sys.exit(0)
