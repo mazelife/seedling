34c15825e1e5efba7ed9500c84c4ce3a209619cd
mazelife <james@mazelife.com>
2024-01-28 11:43:23 -0500

    small improvements to climate app

--- seedling/climate/management/commands/cleanup.py
+++ seedling/climate/management/commands/cleanup.py
@@ -1,6 +1,6 @@
 import sys
 import time
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from django.core.management.base import BaseCommand, CommandParser
 from django.utils import timezone
@@ -10,21 +10,24 @@ from seedling.climate.models import ClimateReading
 
 
 class Command(BaseCommand):
+    help = "Remove climate readings more than N days old."
 
-    help = 'Remove climate readings more than N days old.'
+    @staticmethod
+    def fmt_time(time: datetime) -> str:
+        return time.strftime("%Y-%m-%d %H:%M")
 
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument("--days-old", default=180, type=int, help="Remove readings older than N days")
 
     def handle(self, *args, **options):
         limit = timezone.now() - timedelta(days=options["days_old"])
-        readings: list[ClimateReading] = ClimateReading.options.filter(created__lte=limit).all()
+        readings: list[ClimateReading] = ClimateReading.objects.filter(created__lte=limit).all()
         count = len(readings)
         start, stop = minmax(readings, key=lambda r: r.created)
-        sys.stdout.write(
-            f"This will remove {count} readings between {start.created.isoformat()} and {stop.created.isoformat()}."
-        )
-        response = input('Type "yes" to delete.')
+        start_label = self.fmt_time(start.created)
+        stop_label = self.fmt_time(stop.created)
+        sys.stdout.write(f"This will remove {count:,} readings between {start_label} and {stop_label}.")
+        response = input('Type "yes" to delete:\n')
         if response == "yes":
             ClimateReading.options.filter(created__lte=limit).delete()
             sys.stdout.write("Cleanup complete.")
